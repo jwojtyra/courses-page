c39e54c54d9dfefb7ebbe8f62999a7dbd5ab05e3
cichy380 <marcin.dobroszek@gmail.com>
2021-02-20 17:52:04 +0100

    add AboutComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import {MaterialModule} from './material.module';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import { GameComponent } from './components/game/game.component';
 import { CoverComponent } from './components/cover/cover.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GameComponent,
     CoverComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
